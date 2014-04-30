d4c4ab65cf34f257283f0d48466b3588cb33c3dc
Andy Kwong <andyk314@gmail.com>
2014-04-29 19:17:34 -0700

    create json file, add index and show views, create links, convert date info to correct format

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,4 +2,27 @@ class ApplicationController < ActionController::Base
   # Prevent CSRF attacks by raising an exception.
   # For APIs, you may want to use :null_session instead.
   protect_from_forgery with: :exception
+
+  	helper_method :date_converter, :month_converter, :day_converter, :weekday_converter
+
+	def date_converter(date)
+		t = Time.at(date.to_s[0..-4].to_i).utc.to_datetime
+		t.strftime('%B %e')
+	end
+
+	def month_converter(date)
+		t = Time.at(date.to_s[0..-4].to_i).utc.to_datetime
+		t.strftime('%b')
+	end
+
+	def day_converter(date)
+		t = Time.at(date.to_s[0..-4].to_i).utc.to_datetime
+		t.strftime('%e')
+	end
+
+	def weekday_converter(date)
+		t = Time.at(date.to_s[0..-4].to_i).utc.to_datetime
+		t.strftime('%a')
+	end
+
 end
