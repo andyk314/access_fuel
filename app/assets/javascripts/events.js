283d1dafdb567d6b157969d970a28cd032b9b88a
Andy Kwong <andyk314@gmail.com>
2014-05-03 14:14:34 -0700

    Fix favorite

--- app/assets/javascripts/events.js
+++ app/assets/javascripts/events.js
@@ -34,17 +34,12 @@ function  addYellowClass() {
 	
 	for (var i=0; i<d.length; i++) {
 		var start = String(d[i].onclick).lastIndexOf('e') + 2;
-		var end = String(d[i].onclick).lastIndexOf(' ') + 1;
-		var fav_id = parseInt(String(d[i].onclick).slice(start, end)) // Grabs id
-		// console.log( fav_id );
+		var end = String(d[i].onclick).lastIndexOf(' ');
+		var fav_id = parseInt(String(d[i].onclick).slice(start, end)); // Grabs id
 
-		if (document.cookie.indexOf(fav_id)!= -1) {
-			var start = String(d[i].onclick).lastIndexOf('e') + 2;
-			var end = String(d[i].onclick).lastIndexOf(' ') + 1;
-			var fav_id = parseInt(String(d[i].onclick).slice(start, end))			
+		if (document.cookie.indexOf(fav_id)!= -1) {		
 			d[i].className += " yellow";
 			matches.push(fav_id);
-		// 	}d[1].className += " yellow";
 		}
 	} 	console.log("mat1" + matches);	
 								
