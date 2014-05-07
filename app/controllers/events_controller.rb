e725d81b138431ead2d8f407f04cec0b2b358929
Andy Kwong <andyk314@gmail.com>
2014-05-06 18:12:16 -0700

    add flash error message for favorites when user has not ever saved any favorites

--- app/controllers/events_controller.rb
+++ app/controllers/events_controller.rb
@@ -10,14 +10,18 @@ class EventsController < ApplicationController
   end
 
   def favorite
-  		@info = cookies[:id].split('-')
-  		@events = Event.all.where(id: @info)
+    @info = cookies[:id]
+    if @info == nil
+      flash[:error] = "You don't have any events saved yet. Please select events of interest to you."
+      redirect_to events_path
+    else
+    	@info = cookies[:id].split('-')
+    	@events = Event.all.where(id: @info)        
+    end
   end
 
   def accordian
     time = Time.now.to_i.to_s
     @events = Event.all.where("date > ?", time).order("date ASC")
   end
-
-
 end
