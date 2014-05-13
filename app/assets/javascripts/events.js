9b3853d6e3bb8bebaeac503591f7dbf63beb07f0
Andy Kwong <andyk314@gmail.com>
2014-05-13 02:11:10 -0700

    Fix cookies system

--- app/assets/javascripts/events.js
+++ app/assets/javascripts/events.js
@@ -8,42 +8,47 @@ function setCookie(id){
 		var news = subs.replace(id + "-", "");
 		document.cookie = news;
 	};
-	// console.log("ID: " + id);
-	// console.log("COOKIE: " + document.cookie);
+	console.log("COOKIE Added: " + id);
 };
 
-// #########Test to see if any cookies exists############
+// ###### Delete Cookie ###########
+function deleteCookie(id) {
+	// var now = new Date();
+	var id_to_be_deleted = "id" + id + "=" + "; " + "expires=Thu, 01 Jan 1970 00:00:00 GMT"
+	console.log("COOKIE Deleted: " + id_to_be_deleted);
+	document.cookie = id_to_be_deleted;
+}
+
+
+// ######### Test to see if any cookies exists ############
 function checkCookie(id){
-	if(document.cookie.indexOf('id=')== -1){
-		// console.log("checkCookie");
-		// document.cookie = "id=-";
-}
-	setCookie(id);
+	if(document.cookie.indexOf('id' + id)== -1){	
+		console.log("COOKIE: " + document.cookie);
+		setCookie(id);
+	}
+	else {
+		deleteCookie(id);
+	}
 	
 }
 
 
-
-
-// // ######## Add class yellow in index page if it's a favorite event
-						
-// function  addYellowClass() {
-// 	console.log("Start");
-// 	var matches = []	
-// 	var d = document.getElementsByClassName('glyphicon-star');
+// ######## Add class yellow in index page if it's a favorite event
+function  addYellowClass() {
+	var matches = []	
+	var d = document.getElementsByClassName('glyphicon-star');
 	
-// 	for (var i=0; i<d.length; i++) {
-// 		var start = String(d[i].onclick).lastIndexOf('e') + 2;
-// 		var end = String(d[i].onclick).lastIndexOf(' ');
-// 		var fav_id = parseInt(String(d[i].onclick).slice(start, end)); // Grabs id
+	for (var i=0; i<d.length; i++) {
+		var start = String(d[i].onclick).lastIndexOf('e') + 2;
+		var end = String(d[i].onclick).lastIndexOf(' ');
+		var fav_id = parseInt(String(d[i].onclick).slice(start, end));
 
-// 		if (document.cookie.indexOf('-' + fav_id + '-')!= -1) {		
-// 			d[i].className += " yellow";
-// 			matches.push(fav_id);
-// 		}
-// 	} 	console.log("cookies:" + matches);	
-								
-// }						
+		if (document.cookie.indexOf('id' + fav_id)!= -1) {		
+			d[i].className += " yellow";
+			matches.push(fav_id);
+		}
+	}							
+}						
 
 
 
