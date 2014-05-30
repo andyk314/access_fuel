589037e56a449bdd8e09b821a73b8eea67b028cd
ElenaHoshizaki <elena.hoshizaki@gmail.com>
2014-05-29 21:36:10 -0700

    fix homepage

--- app/assets/javascripts/home.js
+++ app/assets/javascripts/home.js
@@ -1,6 +1,6 @@
 $(function() {
   var body = $('#welcomeBackground');
-  var backgrounds = ['url(http://static.jsbin.com/images/jsbin_static.png)', 'url(http://static.jsbin.com/images/popout.png)'];
+  var backgrounds = ['url(http://wallpic.us/wp-content/uploads/2014/04/LA-City-At-Night-Wallpaper.jpg)', 'url(http://mandarincommunicationsgroup.com/wp-content/uploads/2014/01/los-angeles.jpg)', 'url(http://mixhdwallpapers.com/wp-content/uploads/2014/03/311.jpg)'];
 var current = 0;
 
 function nextBackground() {
