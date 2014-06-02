fab1c119137e6a5da8a621563cfc8ed2727dc3b9
ElenaHoshizaki <elena.hoshizaki@gmail.com>
2014-06-01 19:12:28 -0700

    edit homepage

--- app/assets/javascripts/home.js
+++ app/assets/javascripts/home.js
@@ -2,7 +2,7 @@
 
 $(function() {
   var body = $('#welcomeBackground');
-  var backgrounds = ['url(http://wallpic.us/wp-content/uploads/2014/04/LA-City-At-Night-Wallpaper.jpg)', 'url(http://mandarincommunicationsgroup.com/wp-content/uploads/2014/01/los-angeles.jpg)', 'url(http://mixhdwallpapers.com/wp-content/uploads/2014/03/311.jpg)'];
+  var backgrounds = ['url(http://www.johnmaclean.com/data/photos/222_1_mg_6129_livebooks.jpg) 20% 0px no-repeat fixed', 'url(http://mandarincommunicationsgroup.com/wp-content/uploads/2014/01/los-angeles.jpg)', 'url(http://mixhdwallpapers.com/wp-content/uploads/2014/03/311.jpg)', 'url(http://www.edelsonphotography.com/data/photos/110_1zuma__dsc8462.jpg)'];
 var current = 0;
 
 function nextBackground() {
@@ -11,8 +11,8 @@ function nextBackground() {
     backgrounds[current = ++current % backgrounds.length]
  );
 
- setTimeout(nextBackground, 5000);
+ setTimeout(nextBackground, 8000);
  }
- setTimeout(nextBackground, 5000);
+ setTimeout(nextBackground, 8000);
    body.css('background', backgrounds[0]);
  });
