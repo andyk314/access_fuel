cba311158ba9b576a405f1ff09b43b458aef904d
ElenaHoshizaki <elena.hoshizaki@gmail.com>
2014-06-01 23:02:16 -0700

    add navbar revisions

--- app/assets/javascripts/home.js
+++ app/assets/javascripts/home.js
@@ -2,7 +2,7 @@
 
 $(function() {
   var body = $('#welcomeBackground');
-  var backgrounds = ['url(http://www.johnmaclean.com/data/photos/222_1_mg_6129_livebooks.jpg) 20% 0px no-repeat fixed', 'url(http://mandarincommunicationsgroup.com/wp-content/uploads/2014/01/los-angeles.jpg)', 'url(http://mixhdwallpapers.com/wp-content/uploads/2014/03/311.jpg)', 'url(http://www.edelsonphotography.com/data/photos/110_1zuma__dsc8462.jpg)'];
+  var backgrounds = ['url(http://www.johnmaclean.com/data/photos/222_1_mg_6129_livebooks.jpg) 20% 0px no-repeat fixed', 'url(http://mandarincommunicationsgroup.com/wp-content/uploads/2014/01/los-angeles.jpg)', 'url(http://mixhdwallpapers.com/wp-content/uploads/2014/03/311.jpg)', 'url(http://www.edelsonphotography.com/data/photos/110_1zuma__dsc8462.jpg)', 'url(http://flourishlikethepalm.files.wordpress.com/2013/08/photopalm-trees.jpg)'];
 var current = 0;
 
 function nextBackground() {
