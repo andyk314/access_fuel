c5d95bd54a791e4d59ffdf974191c2acee72dceb
ElenaHoshizaki <elena.hoshizaki@gmail.com>
2014-06-11 21:55:08 -0700

    Update homepage

--- app/assets/javascripts/home.js
+++ app/assets/javascripts/home.js
@@ -1,18 +1,18 @@
 // Changing background image on welcome page
 
-$(function() {
-  var body = $('#welcomeBackground');
-  var backgrounds = ['url(http://www.johnmaclean.com/data/photos/222_1_mg_6129_livebooks.jpg) 20% 0px no-repeat fixed', 'url(http://mandarincommunicationsgroup.com/wp-content/uploads/2014/01/los-angeles.jpg)', 'url(http://mixhdwallpapers.com/wp-content/uploads/2014/03/311.jpg)', 'url(http://www.edelsonphotography.com/data/photos/110_1zuma__dsc8462.jpg)', 'url(http://flourishlikethepalm.files.wordpress.com/2013/08/photopalm-trees.jpg)'];
-var current = 0;
+// $(function() {
+//   var body = $('#welcomeBackground');
+//   var backgrounds = ['url(http://www.johnmaclean.com/data/photos/222_1_mg_6129_livebooks.jpg) 20% 0px no-repeat fixed', 'url(http://mandarincommunicationsgroup.com/wp-content/uploads/2014/01/los-angeles.jpg)', 'url(http://mixhdwallpapers.com/wp-content/uploads/2014/03/311.jpg)', 'url(http://www.edelsonphotography.com/data/photos/110_1zuma__dsc8462.jpg)', 'url(http://flourishlikethepalm.files.wordpress.com/2013/08/photopalm-trees.jpg)'];
+// var current = 0;
 
-function nextBackground() {
-  body.css(
-   'background',
-    backgrounds[current = ++current % backgrounds.length]
- );
+// function nextBackground() {
+//   body.css(
+//    'background',
+//     backgrounds[current = ++current % backgrounds.length]
+//  );
 
- setTimeout(nextBackground, 8000);
- }
- setTimeout(nextBackground, 8000);
-   body.css('background', backgrounds[0]);
- });
+//  setTimeout(nextBackground, 8000);
+//  }
+//  setTimeout(nextBackground, 8000);
+//    body.css('background', backgrounds[0]);
+//  });
