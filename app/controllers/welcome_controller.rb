4a2b18217df53c14eb6e10d673bf7e06cffcce91
Andy Kwong <andyk314@gmail.com>
2014-07-01 08:08:23 -0700

    Refactor css files using body classes.

--- app/controllers/welcome_controller.rb
+++
@@ -1,7 +0,0 @@
-class WelcomeController < ApplicationController
-  def index
-  	time = Time.now.to_i.to_s
-  	@events = Event.all.where("date > ?", time).order("date ASC")
-  	@upcoming_events = Event.all.where("date > ?", time).order("date ASC").limit(3)
-  end
-end
