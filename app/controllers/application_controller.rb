b573e99c0ecf9917684f1f2e2e760f7268cfae62
cwmacken <cwmacken@gmail.com>
2014-07-03 19:14:44 -0700

    user can update their organizer status

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -44,5 +44,6 @@ class ApplicationController < ActionController::Base
 
   def configure_permitted_parameters
     devise_parameter_sanitizer.for(:sign_up) << :meetup_organizer
+    devise_parameter_sanitizer.for(:account_update) << :meetup_organizer
   end	
 end
