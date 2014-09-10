0c11d2e0fe42c3ebd1876dd81dcf63a921139aa2
AdamHKaplan <AdamHKaplan@gmail.com>
2014-09-09 20:48:48 -0700

    revert routes to what they look like in production

--- config/routes.rb
+++ config/routes.rb
@@ -4,12 +4,11 @@ Rails.application.routes.draw do
   post 'login' => 'sessions#create'
   get 'logout' => 'sessions#destroy'
   get 'users/setting', to: 'users#setting', :as => 'settings'
-  get 'users/new' => 'users#new', :as => "new_user"
   post 'users/setting', to: 'users#customize'
   get 'events/favorite', to: 'events#favorite', via:[:get], as: 'favorite'
   get '/ga' => 'application#ga'
 
-  resources :users, only: [ :create, :test ] #test action missing from controller?
+  resources :users, only: [ :create, :new, :test ] #test action missing from controller?
   resources :events, only: [ :index, :show ]
   resources :sessions, only: [ :create ]
   resources :password_resets
