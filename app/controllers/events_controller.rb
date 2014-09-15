79848665ee0088540fd829b1035764b70aea8712
AdamHKaplan <AdamHKaplan@gmail.com>
2014-09-15 16:18:09 -0700

    user should see alert that an event has been removed from (not added to) favorites if user is removing an event from favorites

--- app/controllers/events_controller.rb
+++ app/controllers/events_controller.rb
@@ -3,6 +3,8 @@ class EventsController < ApplicationController
   before_action :current_user
   def index
     #Event.seeder
+    @events = Event.all_events_by_asc_order
+=begin
     if params[:time_period] == "all"
       @events = Event.all_events_by_asc_order
     elsif params[:time_period] == "today"
@@ -42,6 +44,7 @@ class EventsController < ApplicationController
     else
       @events = Event.all_events_by_asc_order
     end
+=end
 
     respond_to do |format|
       format.js
