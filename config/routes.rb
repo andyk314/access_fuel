a8f644dac259b2b7d51376ad356d1bfd51e3224f
Andy Kwong <andyk314@gmail.com>
2014-10-17 01:53:00 -0700

    Add event new page with corresponding routes.

--- config/routes.rb
+++ config/routes.rb
@@ -9,7 +9,15 @@ Rails.application.routes.draw do
   get '/ga' => 'application#ga'
 
   resources :users, only: [ :create, :new, :test ] #test action missing from controller?
-  resources :events, only: [ :index, :show ]
+  # resources :events, only: [ :index, :show ]
+
+  scope controller: :events, path: 'events' do
+    get '', action: :index, as: 'events_home'
+    get 'new_event', action: :new, as: 'add_new_event'
+    post 'new_event', action: :create
+    get ':id', action: :show
+  end
+
   resources :sessions, only: [ :create ]
   resources :password_resets
 
